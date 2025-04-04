6505ef4f0634fa852af726b5b9f817de05fe2d32
Ryan Alqasemi <alqasemir@usf.edu>
2025-04-04 18:42:21 -0400

    fix 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Greeting from './components/Greeting';
+import Greeting from './assets/Greeting';
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
